098e2d9b3304dd2b5e11318e3fcf7430e5f9b8df
Paulo McNally <paulomcnally@gmail.com>
2015-04-22 12:58:42 -0600

    added web app

---
+++ web.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+  "os"
+  "io/ioutil"
+  "github.com/rs/cors"
+)
+
+func main() {
+
+  h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+    data, err := ioutil.ReadFile("./data.json")
+    fmt.Fprintln(w, string(data))
+    if err != nil {
+      panic(err)
+    }
+  })
+
+  // cors.Default() setup the middleware with default options being
+  // all origins accepted with simple methods (GET, POST). See
+  // documentation below for more options.
+  handler := cors.Default().Handler(h)
+  http.ListenAndServe(":"+os.Getenv("PORT"), handler)
+}
